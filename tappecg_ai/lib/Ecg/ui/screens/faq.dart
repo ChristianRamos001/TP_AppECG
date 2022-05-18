42afb5496d33296b98e8cac2f33e7630328765c0
cledmir <56932765+cledmir@users.noreply.github.com>
2022-05-18 00:15:28 -0500

    fix

--- tappecg_ai/lib/Ecg/ui/screens/faq.dart
+++ tappecg_ai/lib/Ecg/ui/screens/faq.dart
@@ -54,45 +54,47 @@ class _FaqState extends State<Faq> {
     return Scaffold(
         appBar: CustomAppbar(),
         drawer: NavBar(),
-        body: Column(children: [
-          SizedBox(
-            height: 2,
-          ),
-          Padding(
-            padding: EdgeInsets.all(15),
-            child: Container(
-              child: Text("Preguntas Frecuentes",
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.green
+        body: SingleChildScrollView(
+          child: Column(children: [
+            SizedBox(
+              height: 2,
+            ),
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: Container(
+                child: Text("Preguntas Frecuentes",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.green
+                  ),
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 2,
-          ),
-          ExpansionPanelList(
-            expansionCallback: (int index, bool isExpanded) {
-              setState(() {
-                faqs[index].isExpanded = !isExpanded;
-              });
-            },
-            children: faqs.map<ExpansionPanel>((Item item) {
-              return ExpansionPanel(
-                headerBuilder: (BuildContext context, bool isExpanded) {
-                  return ListTile(
-                    title: Text(item.headerValue),
-                  );
-                },
-                body: ListTile(
-                  title: Text(item.expandedValue),
-                ),
-                isExpanded: item.isExpanded,
-              );
-            }).toList(),
-          ),
-        ]));
+            SizedBox(
+              height: 2,
+            ),
+            ExpansionPanelList(
+              expansionCallback: (int index, bool isExpanded) {
+                setState(() {
+                  faqs[index].isExpanded = !isExpanded;
+                });
+              },
+              children: faqs.map<ExpansionPanel>((Item item) {
+                return ExpansionPanel(
+                  headerBuilder: (BuildContext context, bool isExpanded) {
+                    return ListTile(
+                      title: Text(item.headerValue),
+                    );
+                  },
+                  body: ListTile(
+                    title: Text(item.expandedValue),
+                  ),
+                  isExpanded: item.isExpanded,
+                );
+              }).toList(),
+            ),
+          ]),
+        ));
   }
 }
